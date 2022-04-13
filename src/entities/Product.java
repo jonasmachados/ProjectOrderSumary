82247b401f41c2e79fd9a91f5b8c830e29c9b253
jonasmachados <jonasmachado.ti@gmail.com>
2022-04-13 16:12:39 -0300

    Created class Order Status

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
-/**
- *
- * @author Jonas
- */
 public class Product {
+
+    private String name;
+    private Double price;
+
+    //CONSTRUCTOR
+    public Product() {
+    }
     
+    //CONSTRUCTOR
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+    //GET AND SET
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+
 }
