c6b1ceef4d6b8d339f54b69a962f307178cfb29c
jonasmachados <jonasmachado.ti@gmail.com>
2022-04-13 14:37:15 -0300

    Created class Product

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
-/**
- *
- * @author Jonas
- */
+import java.util.Date;
+
 public class Client {
+
+    private String name;
+    private String email;
+    private Date birthDate;
+
+    //CONSTRUCTOR
+    public Client() {
+    }
+
+    //CONSTRUCTOR
+    public Client(String name, String email, Date birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    //GET AND SET
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
     
 }
